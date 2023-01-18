43cfbdb9e26586d869d1afbc44f4ddf8747a53c5
ras0q <asymptote.k.k@gmail.com>
2023-01-19 01:56:26 +0900

    :fire: remove duplications

---
+++ remove-duplication.js
@@ -0,0 +1,26 @@
+import fs from 'fs'
+
+const allTitles = []
+
+const datadir = './data/songs'
+const yearSongs = fs.readdirSync(datadir)
+for (const yearSong of yearSongs) {
+  /** @type {Array<{artist: string, title: string, lyric: string | null}>} */
+  const songs = JSON.parse(fs.readFileSync(`${datadir}/${yearSong}`, 'utf-8'))
+  const uniqueSongs = []
+
+  for (const song of songs) {
+    if (allTitles.indexOf(song.title) === -1) {
+      allTitles.push(song.title)
+      uniqueSongs.push(song)
+    } else {
+      console.log('dup: ', song.title)
+    }
+  }
+
+  fs.writeFileSync(
+    `${datadir}/${yearSong}`,
+    JSON.stringify(uniqueSongs, null, 2)
+  )
+  console.log(`Finished ${yearSong}`)
+}
